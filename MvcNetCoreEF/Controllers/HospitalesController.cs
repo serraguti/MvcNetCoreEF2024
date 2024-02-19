953872675ba049e0f99fd75aa1d3fdbd16092ca6
serraguti <pacoserranox@gmail.com>
2024-02-19 13:41:50 +0100

    commit

--- MvcNetCoreEF/Controllers/HospitalesController.cs
+++ MvcNetCoreEF/Controllers/HospitalesController.cs
@@ -18,5 +18,44 @@ namespace MvcNetCoreEF.Controllers
             List<Hospital> hospitales = this.repo.GetHospitales();
             return View(hospitales);
         }
+
+        public IActionResult Details(int idhospital)
+        {
+            Hospital hospital = this.repo.FindHospital(idhospital);
+            return View(hospital);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Hospital hospital)
+        {
+            this.repo.InsertHospital(hospital.IdHospital, hospital.Nombre
+                , hospital.Direccion, hospital.Telefono, hospital.Camas);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            this.repo.DeleteHospital(id);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Hospital hospital = this.repo.FindHospital(id);
+            return View(hospital);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Hospital hospital)
+        {
+            this.repo.UpdateHospital(hospital.IdHospital, hospital.Nombre
+                , hospital.Direccion, hospital.Telefono, hospital.Camas);
+            return RedirectToAction("Index");
+        }
     }
 }
--- MvcNetCoreEF/Repositories/RepositoryHospital.cs
+++ MvcNetCoreEF/Repositories/RepositoryHospital.cs
@@ -1,4 +1,5 @@
-﻿using MvcNetCoreEF.Data;
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using MvcNetCoreEF.Data;
 using MvcNetCoreEF.Models;
 
 namespace MvcNetCoreEF.Repositories
@@ -18,6 +19,58 @@ namespace MvcNetCoreEF.Repositories
             var consulta = from datos in this.context.Hospitales
                            select datos;
             return consulta.ToList();
+        }
+
+        public Hospital FindHospital(int idHospital)
+        {
+            var consulta = from datos in this.context.Hospitales
+                           where datos.IdHospital == idHospital
+                           select datos;
+            //PODRIA SER QUE NO ENCONTRASEMOS HOSPITAL POR EL ID
+            //TENEMOS UN METODO QUE ES FirstOrDefault() QUE DEVUELVE 
+            //EL PRIMER REGISTRO EL VALOR POR DEFECTO DEL MODEL
+            return consulta.FirstOrDefault();
+        }
+
+        public void InsertHospital(int idHospital, string nombre, string direccion
+            , string telefono, int camas)
+        {
+            //INSTANCIAR NUESTRO MODEL
+            Hospital hospital = new Hospital();
+            //ASIGNAMOS LAS PROPIEADES
+            hospital.IdHospital = idHospital;
+            hospital.Nombre = nombre;
+            hospital.Direccion = direccion;
+            hospital.Telefono = telefono;
+            hospital.Camas = camas;
+            //AÑADIMOS EL MODEL A NUESTRA COLECCION DbSet
+            this.context.Hospitales.Add(hospital);
+            //ALMACENAMOS LOS DATOS EN LA BASE DE DATOS
+            this.context.SaveChanges();
+        }
+
+        public void DeleteHospital(int idHospital)
+        {
+            //BUSCAMOS EL MODEL HOSPITAL PARA ELIMINARLO
+            Hospital hospital = this.FindHospital(idHospital);
+            //ELIMINAMOS EL OBJETO DEL DbSet
+            this.context.Hospitales.Remove(hospital);
+            //ALMACENAMOS LOS CAMBIOS EN LA BASE DE DATOS
+            this.context.SaveChanges();
+        }
+
+        public void UpdateHospital(int id, string nombre, 
+            string direccion, string telefono, int camas)
+        {
+            //BUSCAMOS EL HOSPITAL POR SU ID
+            Hospital hospital = this.FindHospital(id);
+            //MODIFICAMOS SUS PROPIEDADES
+            hospital.Nombre = nombre;
+            hospital.Direccion = direccion;
+            hospital.Telefono = telefono;
+            hospital.Camas = camas;
+            //DIRECTAMENTE MODIFICAMOS LA BASE DE DATOS
+            this.context.SaveChanges();
         }
     }
 }
